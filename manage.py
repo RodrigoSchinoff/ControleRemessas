81b47b046d47a6d86dd2c6c17d7a73ebaf600060
andrea <rodrigo.scholiveira@gmail.com>
2025-11-21 23:51:37 -0300

    Admin automático temporário

--- manage.py
+++ manage.py
@@ -7,6 +7,19 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'controle_remessas.settings')
+
+    # --- CRIAR SUPERUSER SE NÃO EXISTIR (TEMPORÁRIO) ---
+    if os.environ.get("AUTO_SUPERUSER") == "1":
+        from django.contrib.auth import get_user_model
+        User = get_user_model()
+        if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser(
+                username="admin",
+                email="admin@admin.com",
+                password="admin123"
+            )
+    # ---------------------------------------------------
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
