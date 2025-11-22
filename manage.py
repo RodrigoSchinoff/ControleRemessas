cf04b90fc140c1d26eeb95495cc61a0771cc2293
andrea <rodrigo.scholiveira@gmail.com>
2025-11-22 00:05:43 -0300

    Admin automático temporário

--- manage.py
+++ manage.py
@@ -8,6 +8,17 @@ def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'controle_remessas.settings')
 
+    try:
+        from django.core.management import execute_from_command_line
+        import django
+        django.setup()
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+
     # --- CRIAR SUPERUSER SE NÃO EXISTIR (TEMPORÁRIO) ---
     if os.environ.get("AUTO_SUPERUSER") == "1":
         from django.contrib.auth import get_user_model
@@ -20,14 +31,6 @@ def main():
             )
     # ---------------------------------------------------
 
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
     execute_from_command_line(sys.argv)
 
 
